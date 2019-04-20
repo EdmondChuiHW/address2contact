bcca21a21502d4748164e086066a1d91ebb62250
edmondchui <chui.hinwai@gmail.com>
2019-04-20 15:42:21 -0600

    +cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const index = require('./routes/index');
 const address2councillor = require('./routes/address2councillor');
 
+app.use(cors());
 app.use('/', index);
 app.use('/address2councillor', address2councillor);
 
