7bfb324e294b7b1dac0c5ca83bffd49aed104a63
edmondchui <chui.hinwai@gmail.com>
2019-04-21 22:51:08 -0600

    +add emdonton bias

--- services/google-maps.service.js
+++ services/google-maps.service.js
@@ -1,12 +1,25 @@
 const {pipe, ifElse, isEmpty, always, juxt, either, complement, isNil, prop, head, then, path, propSatisfies} = require("ramda");
 const turf = require('@turf/turf');
 
+// Apr 21 2019 https://googlemaps.github.io/google-maps-services-js/docs/GoogleMapsClient.html#.geocode
+const edmontonBounds = {
+  south: 53.414896,
+  west: -113.709007,
+  north: 53.676185,
+  east: -113.296081,
+};
+
 const googleMapsClient = require('@google/maps').createClient({
   key: process.env.GOOGLE_MAPS_API_KEY,
   Promise: Promise,
 });
 
-const googleGeoWithAddress = address => googleMapsClient.geocode({address}).asPromise();
+const googleGeoWithAddress = (address = '', bounds = edmontonBounds) => (
+  googleMapsClient.geocode({
+    address,
+    bounds,
+  }).asPromise()
+);
 
 const parseGooglePoint = pipe(
   juxt([prop('lng'), prop('lat')]),
