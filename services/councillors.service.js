f16cb52f46140d2fb55ea4ce1fa4fda4a070edd7
edmondchui <chui.hinwai@gmail.com>
2019-04-21 22:31:27 -0600

    +resolve photo

--- services/councillors.service.js
+++ services/councillors.service.js
@@ -1,10 +1,34 @@
-const {pick, head, ifElse, pipe, either, isEmpty, isNil, always, then} = require('ramda');
+const {pick, head, ifElse, prop, when, flip, has, pipe, either, isEmpty, isNil, always, then} = require('ramda');
 const requests = require('request-promise-native');
+
+const knownBrokenPhotoUrls = {
+  "http://www.edmonton.ca/city_government/documents/Images/Mayor_Don_Iveson200X250.jpg": "https://www.edmonton.ca/city_government/documents/Mayor-Headshot_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Andrew_Knack200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-1-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Bev_Esslinger200X250(1).jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-2-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Dave_Loken200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-3-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Ed_Gibbons200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-4-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Michael_Oshry200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-5-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Scott_McKeen200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-6-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Tony_Caterina200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-7-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Ben_Henderson200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-8-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Bryan_Anderson200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-9-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Michael_Walters200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-10-councillor_800x494_rdax_500x309.jpg",
+  "http://www.edmonton.ca/city_government/documents/Images/Councillor_Mike_Nickel200X250.jpg": "https://www.edmonton.ca/city_government/documents/Images/ward-11-councillor_800x494_rdax_500x309.jpg",
+  "": "https://www.edmonton.ca/city_government/documents/Images/ward-12-councillor_800x494_rdax_500x309.jpg",
+};
+
+const isPropBrokenUrl = flip(has)(knownBrokenPhotoUrls);
+const replaceBrokenUrl = flip(prop)(knownBrokenPhotoUrls);
+
+exports.resolvePhotoUrl = pipe(
+  prop('photo_url'),
+  when(isPropBrokenUrl, replaceBrokenUrl),
+);
 
 exports.mapCouncillor = c => ({
   firstName: c.first_name,
   lastName: c.last_name,
-  photoUrl: c.photo_url,
+  photoUrl: exports.resolvePhotoUrl(c),
   role: c.primary_role,
   ward: c.electoral_area,
   ...pick(['email', 'phone', 'url'], c),
--- services/councillors.service.test.js
+++ services/councillors.service.test.js
@@ -1,6 +1,26 @@
+const {resolvePhotoUrl} = require("./councillors.service");
 const {mapCouncillor} = require("./councillors.service");
 
 describe('councillors service', () => {
+  describe('resolvePhotoUrl', () => {
+    it('should replace Mayor', () => {
+      const actual = resolvePhotoUrl({photo_url: "http://www.edmonton.ca/city_government/documents/Images/Mayor_Don_Iveson200X250.jpg"});
+      const expected = "https://www.edmonton.ca/city_government/documents/Mayor-Headshot_800x494_rdax_500x309.jpg";
+      expect(actual).toEqual(expected);
+    });
+
+    it('should replace Councillor', () => {
+      const actual = resolvePhotoUrl({photo_url: "http://www.edmonton.ca/city_government/documents/Images/Councillor_Bev_Esslinger200X250(1).jpg"});
+      const expected = "https://www.edmonton.ca/city_government/documents/Images/ward-2-councillor_800x494_rdax_500x309.jpg";
+      expect(actual).toEqual(expected);
+    });
+
+    it('should let others pass thru', () => {
+      const actual = resolvePhotoUrl({photo_url: "http://www.edmonton.ca/"});
+      const expected = "http://www.edmonton.ca/";
+      expect(actual).toEqual(expected);
+    });
+  });
   describe('mapCouncillor', () => {
     it('should map mayor', () => {
       const sample = {
@@ -26,7 +46,7 @@ describe('councillors service', () => {
         firstName: "Don",
         lastName: "Iveson",
         phone: "7804968100",
-        photoUrl: "http://www.edmonton.ca/city_government/documents/Images/Mayor_Don_Iveson200X250.jpg",
+        photoUrl: "https://www.edmonton.ca/city_government/documents/Mayor-Headshot_800x494_rdax_500x309.jpg",
         url: "http://www.edmonton.ca/city_government/city_organization/mayor/more-about-the-mayor.aspx",
       };
       expect(mapCouncillor(sample)).toEqual(expected);
@@ -56,7 +76,7 @@ describe('councillors service', () => {
         firstName: "Andrew",
         lastName: "Knack",
         phone: "7804968122",
-        photoUrl: "http://www.edmonton.ca/city_government/documents/Images/Councillor_Andrew_Knack200X250.jpg",
+        photoUrl: "https://www.edmonton.ca/city_government/documents/Images/ward-1-councillor_800x494_rdax_500x309.jpg",
         url: "http://www.edmonton.ca/city_government/city_organization/ward-1-councillor.aspx",
       };
       expect(mapCouncillor(sample)).toEqual(expected);
