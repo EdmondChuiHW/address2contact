94fff029a8d5831bb851f298778ae8193ab7eaa6
edmondchui <chui.hinwai@gmail.com>
2019-04-22 01:38:52 -0600

    +cors

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const index = require('./routes/index');
-const address = require('./routes/address');
 const address2councillor = require('./routes/address2councillor');
 
-app.use(cors());
+app.use(cors({origin: "https://chuihinwai.github.io/email-contact/"}));
 app.use('/', index);
-app.use('/address', address);
 app.use('/address2councillor', address2councillor);
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
