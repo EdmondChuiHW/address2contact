94dbf268cff31f6ac2db3e98409d98adf8802eaf
edmondchui <chui.hinwai@gmail.com>
2019-04-28 18:37:21 -0600

    +allow http localhost

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const index = require('./routes/index');
 const address2councillor = require('./routes/address2councillor');
 
-app.use(cors({origin: [/^https:\/\/chuihinwai.github.io/, /^https:\/\/localhost/]}));
+app.use(cors({origin: [/^https:\/\/chuihinwai.github.io/, /^https?:\/\/localhost/]}));
 app.use('/', index);
 app.use('/address2councillor', address2councillor);
 
