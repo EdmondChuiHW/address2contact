e1c218406ba736857c701078559fe3e11517ebe9
edmondchui <chui.hinwai@gmail.com>
2019-04-29 22:43:48 -0600

    *minor refactoring [skip ci]

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const program = require('commander');
 const fs = require('fs');
 const https = require('https');
 
+const {when, always, curry} = require('ramda');
+
 program
   .version('1.0.0')
   .option('-S, --https', 'Use HTTPS to serve localhost. Expects server.cert and server.key in root dir')
   .parse(process.argv);
 exports.isUsingSsl = program.https;
 
-const wrapAppWithSsl = a => exports.isUsingSsl
-  ? https.createServer({
-      key: fs.readFileSync('server.key'),
-      cert: fs.readFileSync('server.cert'),
-    },
-    a,
-  )
-  : a;
+const wrapAppWithSsl = curry(https.createServer)({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert'),
+});
 
-wrapAppWithSsl(app).listen(port, () => {
-  console.log(`Listening on http${app.isUsingSsl ? 's' : ''}://localhost:${port}!`);
+const wrapIfEnabled = when(always(exports.isUsingSsl), wrapAppWithSsl);
+
+wrapIfEnabled(app).listen(port, () => {
+  console.log(`Listening on http${exports.isUsingSsl ? 's' : ''}://localhost:${port}!`);
 });
